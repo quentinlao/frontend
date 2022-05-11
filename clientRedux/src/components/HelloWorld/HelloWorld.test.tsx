6742c6a2953f996fd58651789d8d016aad21ac64
quentin <quentinlao@outlook.com>
2022-05-11 19:36:28 +0200

    feat add mocked test jest

--- clientRedux/src/components/HelloWorld/HelloWorld.test.tsx
+++ clientRedux/src/components/HelloWorld/HelloWorld.test.tsx
@@ -4,19 +4,26 @@ import '@testing-library/jest-dom';
 
 import { HelloWorld, sumFunction } from './HelloWorld';
 import { getByText } from '@testing-library/dom';
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../../api/counter.service';
+import { Provider } from 'react-redux';
 
 afterEach(cleanup);
 
 let container: HTMLElement;
 
 describe('HelloWorld component suites tests', () => {
+    const mockStore = configureStore({
+        reducer: {
+            counter: counterReducer,
+        },
+    });
+
     beforeEach(() => {
         const component = render(
-            <HelloWorld
-                name="Jean"
-                title="Hello world"
-                description="Lorem ipsum"
-            />
+            <Provider store={mockStore}>
+                <HelloWorld name="Jean" title="Hello world" description="Lorem ipsum" />
+            </Provider>
         );
         container = component.container;
     });
@@ -24,8 +31,6 @@ describe('HelloWorld component suites tests', () => {
         expect(sumFunction(1, 2)).toEqual(3);
         expect(container).toBeInTheDocument();
         expect(getByText(container, 'Lorem ipsum')).toBeInTheDocument();
-        expect(
-            screen.getByRole('heading', { name: /Hello world, Jean !/i })
-        ).toBeInTheDocument();
+        expect(screen.getByRole('heading', { name: /Hello world, Jean !/i })).toBeInTheDocument();
     });
 });
